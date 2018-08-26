3bcab36e7f34d01964716ee8dc6b83fd2a4a11e0
RY_2718(Ryo Yoshitani) <ry.3607x5531@gmail.com>
2018-08-26 17:58:17 +0900

    :sparkles: Add: UserToken modelを追加

---
+++ db/migrate/20180826082600_user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :token, :string
+
+    create_table :user_tokens do |t|
+      t.references :user, index: true, foregin_key: true
+      t.string :token
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_175146) do
+ActiveRecord::Schema.define(version: 2018_08_26_082600) do
+
+  create_table "user_tokens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_tokens_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "username"
-    t.string "token"
     t.string "google_uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
