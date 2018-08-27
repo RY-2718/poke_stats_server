d58fc122ce1e26b13f6ba3124256fdb3fa3f52b2
RY_2718(Ryo Yoshitani) <ry.3607x5531@gmail.com>
2018-08-27 12:02:05 +0900

    :sparkles: Add: JWTを発行する機能を追加

--- app/controllers/api/v1/token_controller.rb
+++ app/controllers/api/v1/token_controller.rb
@@ -3,6 +3,17 @@ class Api::V1::TokenController < ApplicationController
   def create
     google_uid = request.env['omniauth.auth']['uid']
 
-    render json: "{uid: #{google_uid}}"
+    user = User.find_or_initialize_by(google_uid: google_uid)
+    if user.new_record?
+      render json: { errors: ['新規ユーザの作成に失敗しました'] }, status: :unauthorized unless user.save
+    end
+
+    user_token = user.user_tokens.new(token: user.create_token)
+    if user_token.save
+      # TODO: どのような形でユーザに返すかは要検討かも
+      render json: { token: user_token.token }
+    else
+      render json: { errors: ['トークンの作成に失敗しました'] }, status: :unauthorized
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many(:user_tokens, dependent: :destroy)
+
+  def create_token
+    JWT.encode({ id: id, iat: Time.zone.now.to_i }, Rails.application.credentials.secret_key_base, 'HS256')
+  end
 end
